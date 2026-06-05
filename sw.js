3757faf682ff79fc4e7608bea43fa719841e2a12
kaizhof <kaizhof@proton.me>
2026-06-05 16:12:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,15 @@
-const CACHE_NAME = 'saza-v1';
+const CACHE_NAME = 'k-v1';
+const urlsToCache = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(['/', '/index.html']);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
